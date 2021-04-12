f7893498944b37512e846e943d390a065ae9ca56
just-sid <be10125@bitmesra.ac.in>
2021-04-13 00:38:42 +0530

    Database Connection established

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-require("dotenv/config");
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Datababse connection established");
+})
 
 app.listen(port, () => {
  console.log(`Server running on port: ${port}`);
