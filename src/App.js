c70768805c8287f30ff29ab25eb2d014e1d7dd3a
just-sid <be10125@bitmesra.ac.in>
2021-04-15 06:30:09 +0530

    Route paths added to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import 'boootstrap/dist/css/bootstrap.min.css'
+
+import Navbar fromm "./components/exercise-list";
 
 function App() {
   return (
-    <div className="App">
-      Hello World 
-    </div>
+    <Router>
+      <Navbar />
+      <br/>
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit:exercisesId"  component={EditExercises} />
+      <Route path="/createExercises"  component={CreateExercises} />
+      <Route path="/createUsers"  component={CreateUsers} />
+    </Router>
   );
 }
 
