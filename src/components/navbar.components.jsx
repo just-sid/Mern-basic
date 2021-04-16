4293fed9c796e653331fb3f357b53710dd5aab5d
just-sid <be10125@bitmesra.ac.in>
2021-04-16 17:27:46 +0530

    Navbar component added

---
+++ src/components/navbar.components.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to='/' className='navbar-brand'>Exercise Tracker</Link>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                    <Link to='/' className='nav-link'>Exercises</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link to='/createExercises' className='nav-link'>Create Exercises</Link>
+                    </li>
+                    <li>
+                    <li className="nav-item">
+                    <Link to='/createUsers' className='nav-link'>Create Users</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        );
+    }
+}
