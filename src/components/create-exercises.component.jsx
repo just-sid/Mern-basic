9d9d3526a3804d4b7cde5790c5f21cc36f375cda
just-sid <be10125@bitmesra.ac.in>
2021-04-19 01:49:31 +0530

    Navbar added

--- src/components/create-exercises.component.jsx
+++ src/components/create-exercises.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CreateExercises extends Component {
      render() { 
--- src/components/exercises-list.component.jsx
+++ src/components/exercises-list.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ExercisesList extends Component {
      render() { 
