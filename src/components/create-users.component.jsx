94ad0bbdd0fb25485b0f2f1f1c021bcccda3bdfe
just-sid <be10125@bitmesra.ac.in>
2021-04-19 05:50:42 +0530

    CreateUsers component

--- src/components/create-users.component.jsx
+++ src/components/create-users.component.jsx
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
 
 class CreateUsers extends Component {
-     render() { 
-         return ( 
-             <div>
-                <p>CreateUsers</p>
-             </div>
-          );
-     }
- }
-  
- export default CreateUsers;
+    constructor(props) {
+        super(props);
+        this.state = { user: '' }
+    }
+
+    onChangeUser = (e) => {
+        this.setState({
+            user: e.target.value
+        });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const user = {user: this.state.user}
+        console.log(user);
+        console.log('New user created');
+        window.location='/';
+    }
+
+    render() { 
+        return ( 
+            <div>
+                <h1>Create new User</h1>
+                <form onSubmit = {this.onSubmit} >
+                <div className='form-group'>
+                        <label>New User: </label>
+                        <input type="text" 
+                            required
+                            className="form-control"
+                            value={this.state.user}
+                            onChange={this.onChangeUser}>
+                            </input>
+                    </div>
+                    <div className="form-group">
+                            <input type="submit" value="Create New User" className="btn btn-primary" />
+                        </div>
+                </form>
+            </div>
+         );
+    }
+}
+ 
+export default CreateUsers;
